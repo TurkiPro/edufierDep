b31fc843b6c843d7531109e427a5198847ca1526
Turki Almalki <iiata767@gmail.com>
2023-06-14 06:12:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const expressValidator = require("express-validator");
 const app = express();
 const dotenv = require("dotenv");
 const router = require("./routes/router");
-const path = require('path');
+const path = require("path");
 
 const PORT = 3000;
 dotenv.config();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
 
-
-router.default(app)
+router.default(app);
 
 app.listen(PORT, () => {
-    console.log("express started on port 3000")
-})
+  console.log("express started on port 3000");
+});
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 
 // For outside url
 mongoose
-    .connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(() => {
-        console.log("Connected to MongoDB");
-    }).catch((err) => {
-        console.log(err);
-    })
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // localhost
 // mongoose.connect(`${process.env.MONGO_URL}`, ()=>{
