ba5f321c06ef97c328fc8c55b1de8c3d4b47bd12
Turki Almalki <iiata767@gmail.com>
2023-06-21 11:08:35 +0300

    Input validation for: assignPoints

--- controllers/point.js
+++ controllers/point.js
@@ -6,42 +6,55 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const service = require("../service/mongo.service");
 const completion = require("./completion");
+const { body, validationResult } = require("express-validator");
 
+const assignPointsValidationRules = [
+  body("amount")
+    .isInt({ min: 0 })
+    .withMessage("Amount must be a positive integer"),
+  body("completionId").isMongoId().withMessage("Invalid completion ID"),
+  body("userId").isMongoId().withMessage("Invalid user ID"),
+];
 
-const assignPoints = async (amount, completionId, userId) =>{
+const assignPoints = async (amount, completionId, userId) => {
+  // Validate input using the defined validation rules
+  await Promise.all(assignPointsValidationRules.map((rule) => rule.run(req)));
 
-    // get the comment text and record post id
-    const points = new Point({
-        amount: amount,
-        userID: userId,
-        completionId: completionId
-    })
-    // save comment
-    try{
-        await points.save();
-        await User.findByIdAndUpdate(userId, { $inc: {'earnedPoints': amount } })
-        console.log(points)
-        return true;
-    }catch(err){
-        console.log(err)
-        return false;
-    }
-    
-}
+  // Check for validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // If there are validation errors, return an error response
+    return res.status(400).json({ errors: errors.array() });
+  }
 
+  // get the comment text and record post id
+  const points = new Point({
+    amount: amount,
+    userID: userId,
+    completionId: completionId,
+  });
+  // save comment
+  try {
+    await points.save();
+    await User.findByIdAndUpdate(userId, { $inc: { earnedPoints: amount } });
+    console.log(points);
+    return res.status(200).json({ message: "Points assigned successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 module.exports = {
+  getAllPoints: (req, res) => {
+    Point.find({}).then((points) => {
+      res.json(points);
+    });
+  },
+  deletePoint: (req, res) => {
+    let id = req.param.id;
 
-
-    getAllPoints: (req, res) =>{
-        Point.find({}).then(points => {res.json(points)})
-    },
-    deletePoint: (req, res) => {
-        let id = req.param.id;
-
-        Point.findByIdAndDelete(id);
-
-    },
-    assignPoints
-
-}
+    Point.findByIdAndDelete(id);
+  },
+  assignPoints,
+};
